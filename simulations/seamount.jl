32f74cf9b2202624ab592c6fb57a6b5c82ec9784
tomaschor <tomaschor@gmail.com>
2026-02-27 23:28:51 +0000

    much simpler

--- simulations/seamount.jl
+++ simulations/seamount.jl
@@ -1,30 +1,21 @@
 using Oceananigans
 using Oceananigans.Units
 
-@info "Starting simulation $simname" Nx Ny Nz dz
-#---
-
 #+++ Base grid
 grid = RectilinearGrid(topology = (Bounded, Periodic, Bounded),
                        size = (8, 8, 8),
                        x = (-1000, 1000),
                        y = (-1000, 1000),
                        z = (-100, 0),
-                       halo = (4, 4, 4),
                        )
-#---
-
-#+++ Drag boundary conditions at bottom
-τᵘ_bottom = FluxBoundaryCondition(0)
-#---
 
 #+++ Open boundary conditions for velocities
-u_west = OpenBoundaryCondition(U∞)
-u_east = OpenBoundaryCondition(U∞; scheme = PerturbationAdvection(inflow_timescale = 2minutes, outflow_timescale = 30minutes))
+u_west = OpenBoundaryCondition(1)
+u_east = OpenBoundaryCondition(1; scheme = PerturbationAdvection(inflow_timescale = 2minutes, outflow_timescale = 30minutes))
 #---
 
 #+++ Assemble BCs
-u_bcs = FieldBoundaryConditions(west=u_west, east=u_east, bottom=τᵘ_bottom)
+u_bcs = FieldBoundaryConditions(west=u_west, east=u_east)
 bcs = (u=u_bcs,)
 #---
 
@@ -38,18 +29,17 @@ simulation = Simulation(model, Δt = 40seconds,
 #+++ Define checkpointer/pickup
 write_ckpt = true
 
-checkpointer_prefix = "ckpt.$simname"
+checkpointer_prefix = "ckpt.TEST"
 if any(startswith(checkpointer_prefix), readdir("data"))
-    @warn "Checkpoint for $simname found. Assuming this is a pick-up simulation! Setting overwrite_existing=false."
+    @warn "Checkpoint for TEST found. Assuming this is a pick-up simulation! Setting overwrite_existing=false."
     overwrite_existing = false
 else
-    @warn "No checkpoint for $simname found. Setting overwrite_existing=true."
+    @warn "No checkpoint for TEST found. Setting overwrite_existing=true."
     overwrite_existing = true
 end
 
 @info "Setting up checkpointer"
 simulation.output_writers[:ckpt_writer] = Checkpointer(model;
-                                                        dir = "$rundir/data/",
                                                         prefix = checkpointer_prefix,
                                                         schedule = TimeInterval(10minutes),
                                                         overwrite_existing = true,
