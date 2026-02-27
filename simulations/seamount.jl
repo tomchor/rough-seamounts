c48a37902461345cc755735db7f1f05defc96826
tomaschor <tomaschor@gmail.com>
2026-02-27 23:36:05 +0000

    simplify more

--- simulations/seamount.jl
+++ simulations/seamount.jl
@@ -1,5 +1,4 @@
 using Oceananigans
-using Oceananigans.Units
 
 grid = RectilinearGrid(topology = (Bounded, Periodic, Bounded), size = (8, 8, 8), extent = (1, 1, 1))
 
@@ -7,35 +6,14 @@ u_west = OpenBoundaryCondition(1)
 u_east = OpenBoundaryCondition(1; scheme = PerturbationAdvection(inflow_timescale = 2minutes, outflow_timescale = 30minutes))
 u_bcs = FieldBoundaryConditions(west=u_west, east=u_east)
 
-@info "Creating model"
 model = NonhydrostaticModel(grid, boundary_conditions = (u=u_bcs,))
+simulation = Simulation(model, Δt = 1, wall_time_limit = 1)
 
-simulation = Simulation(model, Δt = 40seconds,
-                        wall_time_limit = 1second,
-                        )
-
-#+++ Define checkpointer/pickup
-write_ckpt = true
-
-checkpointer_prefix = "ckpt.TEST"
-if any(startswith(checkpointer_prefix), readdir("data"))
-    @warn "Checkpoint for TEST found. Assuming this is a pick-up simulation! Setting overwrite_existing=false."
-    overwrite_existing = false
-else
-    @warn "No checkpoint for TEST found. Setting overwrite_existing=true."
-    overwrite_existing = true
-end
-
-@info "Setting up checkpointer"
 simulation.output_writers[:ckpt_writer] = Checkpointer(model;
-                                                       prefix = checkpointer_prefix,
-                                                       schedule = TimeInterval(10minutes),
+                                                       schedule = TimeInterval(10),
                                                        overwrite_existing = true,
-                                                       cleanup = true,
-                                                       )
-#---
+                                                       cleanup = true)
 
-#+++ Run simulation
-@info "Starting simulation"
-run!(simulation, pickup=write_ckpt, checkpoint_at_end=write_ckpt)
-#---
+run!(simulation, pickup=false, checkpoint_at_end=true) # Run fresh simulation
+run!(simulation, pickup=true) # Pickup simulation
+
