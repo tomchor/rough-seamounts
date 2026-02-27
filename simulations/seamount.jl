f2d1fb54a9f7b697e7f37baed0e81617d68581b8
tomaschor <tomaschor@gmail.com>
2026-02-27 23:42:37 +0000

    finally something minimal

--- simulations/seamount.jl
+++ simulations/seamount.jl
@@ -1,11 +1,9 @@
 using Oceananigans
 
-grid = RectilinearGrid(topology = (Bounded, Periodic, Bounded), size = (8, 8, 8), extent = (1, 1, 1))
+grid = RectilinearGrid(size = (8, 8, 8), extent = (1, 1, 1))
 
-u_east = OpenBoundaryCondition(0; scheme = PerturbationAdvection())
-u_bcs = FieldBoundaryConditions(west=u_west, east=u_east)
-
-model = NonhydrostaticModel(grid, boundary_conditions = (u=u_bcs,))
+w_bc = FieldBoundaryConditions(bottom=OpenBoundaryCondition(0; scheme = PerturbationAdvection()))
+model = NonhydrostaticModel(grid, boundary_conditions = (w=w_bc,))
 simulation = Simulation(model, Δt = 1, wall_time_limit = 1)
 
 simulation.output_writers[:ckpt_writer] = Checkpointer(model;
@@ -15,4 +13,3 @@ simulation.output_writers[:ckpt_writer] = Checkpointer(model;
 
 run!(simulation, pickup=false, checkpoint_at_end=true) # Run fresh simulation
 run!(simulation, pickup=true) # Pickup simulation
-
