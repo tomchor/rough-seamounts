41fde28dee8015b47077cd30be81549ca74024d5
tomchor <tomaschor@gmail.com>
2026-04-16 13:24:25 -0600

    bugfix with new version

--- simulations/seamount.jl
+++ simulations/seamount.jl
@@ -330,7 +330,7 @@ b_sponge = Relaxation(rate=params.sponge_damping_rate, mask=mask_top, target=bâˆ
 model = NonhydrostaticModel(grid, timestepper = :RungeKutta3,
                             advection = WENO(order=5, minimum_buffer_upwind_order=1), # minimum_buffer_upwind_order=1 necessary for PerturbationAdvection
                             buoyancy = BuoyancyTracer(),
-                            coriolis = FPlane(params.f_0),
+                            coriolis = FPlane(f=params.f_0),
                             tracers = :b,
                             closure = closure,
                             boundary_conditions = bcs,
